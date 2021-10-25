73d4ad118a014adaaa782c5c534b695f2fb8f1f8
Veronika <desatericenkoveronika@gmail.com>
2021-10-25 16:05:10 +0200

    homework1

--- numbers.js
+++ numbers.js
@@ -1,16 +1,14 @@
 const [n, o] = process.argv.slice(2);
 const colors = require('colors');
-/* const n = "è", o = "" */
 
 for(let i = n; i <= o; i++){
         if(i%2 == 0){
             console.log(colors.red(i));
             console.log(colors.yellow(i));
             console.log(colors.green(i));
-            /* console.log(i) */
         }
-        else if(i%2 !== 0){
-            console.log(colors.red("There are no prime numbers in the range "));
+        else if(isNaN(n && o)){
+            console.log(colors.red("Write range of numbers"));
         }
         else if(typeof(n & o) !== "number") {
             console.log(colors.red("ERROR: The argument(s) is not a number"));
