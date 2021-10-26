be2d3f08cfb524aa3ec627dd5224b96766dbed8e
Veronika <desatericenkoveronika@gmail.com>
2021-10-26 13:03:01 +0200

    homework in progress

--- lezione2/index.js
+++ lezione2/index.js
@@ -1,4 +1,5 @@
-console.log("Record 1");
+//1
+/* console.log("Record 1");
 
 setTimeout(() => {
   console.log("Record 2");
@@ -16,7 +17,7 @@ console.log("Record 5");
 
 Promise.resolve().then(() =>
   Promise.resolve().then(() => console.log("Record 6"))
-);
+); */
 
 //В данном примере будет вывод в консоль:
 //Record 1
@@ -33,3 +34,64 @@ Promise.resolve().then(() =>
 //выполнится 'Record 2'
 //затем 'Record 3' и 'Record 4', так как они находятся один внутри другого
 //(одна микрозадача внитри другой мирко задачи);
+
+//2
+//Напишите программу, которая будет принимать на вход несколько аргументов:
+//дату и время в формате «час-день-месяц-год».
+//Задача программы — создавать для каждого аргумента таймер с обратным отсчётом:
+//посекундный вывод в терминал состояния таймеров (сколько осталось).
+//По истечении какого-либо таймера, вместо сообщения о том, сколько осталось,
+//требуется показать сообщение о завершении его работы.
+//Важно, чтобы работа программы основывалась на событиях.
+
+//partenza node index.js 25-12-2021 10:00:00
+//console.log('Tempo rimasto: ${data}' )
+//console.clear()
+//anni: 0, mese: 2, giorni: 30, ore: 10, minuti: 34, secondi 18
+//anni: 0, mese: 2, giorni: 30, ore: 10, minuti: 34, secondi 17
+//anni: 0, mese: 2, giorni: 30, ore: 10, minuti: 34, secondi 16
+//anni: 0, mese: 2, giorni: 30, ore: 10, minuti: 34, secondi 15
+//anni: 0, mese: 2, giorni: 30, ore: 10, minuti: 34, secondi 14
+//su un taimer finisce:
+//Timpo è finito!
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+
+function temer() {
+  const timerFuter = Date.parse(process.argv.slice(2));
+  const dateNow = Date.now();
+  let dateDiff = timerFuter - dateNow;
+
+  let yers = Math.floor(dateDiff / (1000 * 60 * 60 * 24 * 30 * 12));
+  let month = Math.floor(dateDiff / (1000 * 60 * 60 * 24 * 30));
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(dateDiff / (1000 * 60 * 60));
+  let mins = Math.floor(dateDiff / (1000 * 60));
+  let secs = Math.floor(dateDiff / 1000);
+
+  let y = yers;
+  let mt = month - yers * 12;
+  let d = days - month * 30;
+  let h = hours - days * 24;
+  let m = mins - hours * 60;
+  let s = secs - mins * 60;
+
+/*   console.log(
+    `yers: ${y}, month: ${mt}, days: ${d}, hours: ${h}, mints: ${m},seconds: ${s}`
+  ); */
+  const run = async () => {
+      await new Promise((resolve) => setInterval(resolve, 1000));
+      emitter.emit("error", new Error("woops!"));
+      emitter.on("event", console.log(
+          `yers: ${y}, month: ${mt}, days: ${d}, hours: ${h}, mints: ${m},seconds: ${s}`
+        ));
+      run();
+    };
+}
+
+  emitter.emit("event");
+emitter.on("error", (err) => {
+  console.error("woops! there was an error");
+});
+
+temer();
